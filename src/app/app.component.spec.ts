9c46cb5656a3062e5781ef47e2644dc0671cbe9a
morninng <mr_morninng_star_dash@yahoo.co.jp>
2018-03-14 16:31:28 +0900

    remove one spec file

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,14 +1,16 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
-import { TestComponent } from './test/test.component'
+import { TestComponent } from './test/test.component';
+import { Test2Component } from './test2/test2.component';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
         AppComponent,
-        TestComponent
+        TestComponent,
+        Test2Component
       ],
     }).compileComponents();
   }));
