0840424d2c67d0d1b98bf9e29ae10ed2f0e57f82
morninng <mr_morninng_star_dash@yahoo.co.jp>
2018-03-14 16:10:56 +0900

    initial add

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,10 +1,14 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+
+import { TestComponent } from './test/test.component'
+
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent,
+        TestComponent
       ],
     }).compileComponents();
   }));
